85d1b63bbf5981733a03f63a535fb4bdb47d314a
ottojoash <louisjoshbricks@gmail.com>
2022-09-22 04:47:44 -0400

    view

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ import { Component } from 'react';
 class App extends Component{
   render(){
     return(
-
+      <View style={styles.root}>
+        <Text>Battle Math</Text>
+      </View>
+      
     );
   }
 }
+
+const styles = StyleSheet.create({
+  root: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 
 export default App;
